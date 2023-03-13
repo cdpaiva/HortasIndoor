eabdb0a99cb94b10ecdcd8d56a851eaf5a5993b2
cdpaiva <cdpaivaa@gmail.com>
2023-03-13 08:17:14 -0400

    Interface para login

---
+++ HortasIndoor.Core/ViewModels/LoginCredentialsViewModel.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HortasIndoor.Core.ViewModels
+{
+    public class LoginCredentialsViewModel
+    {
+        [Required(ErrorMessage = "O campo Username deve ser preenchido")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Por favor, insira seu Password")]
+        public string? Password { get; set; }
+        public string? ErrorMessage { get; set; }
+    }
+}
---
+++ HortasIndoor.Web/Controllers/AccountController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using HortasIndoor.Core.Models;
+using System.Text;
+using Newtonsoft.Json;
+
+namespace HortasIndoor.Web.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginCredentials credentials)
+        {
+            using(var httpClient = new HttpClient())
+            {
+                var content = new StringContent(JsonConvert.SerializeObject(credentials), Encoding.UTF8, "application/json");
+
+                using(var response = await httpClient.PostAsync("http://localhost:5085/api/Auth/Login", content)){
+                    if(response.StatusCode == System.Net.HttpStatusCode.BadRequest)
+                    {
+                        return View();
+                    }
+                    
+                    var apiResponse = await response.Content.ReadAsStringAsync();
+                    ViewBag.Token = apiResponse;
+                    return View();
+
+                }
+            }
+        }
+    }
+}
